9586607e683ad1215314ab5175e1aa70b56538b5
Michael Atlin <mike.atlin@gmail.com>
2014-02-08 13:33:06 -0500

    Rebuilt all controllers as plural, built user signup view, changed user:phone to string

--- seat_yourself/app/models/user.rb
+++ seat_yourself/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :email, :name, :phone, presence: true
+  has_many :reservations
+  has_many :restaurants, through: :reservations
 end
--- seat_yourself/db/schema.rb
+++ seat_yourself/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140207214003) do
+ActiveRecord::Schema.define(version: 20140208182756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20140207214003) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.integer  "phone"
+    t.string   "phone"
     t.string   "password_digest"
     t.string   "name"
     t.datetime "created_at"
