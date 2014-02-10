f96831229e76396d5bc07acbb45931b06cd5dec8
Michael Atlin <mike.atlin@gmail.com>
2014-02-09 21:54:13 -0500

    Basic Restaurant methods added

--- seat_yourself/app/controllers/restaurants_controller.rb
+++ seat_yourself/app/controllers/restaurants_controller.rb
@@ -1,6 +1,35 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+  def new
+    @restaurant = Restaurant.new
+  end
 
-def index
-  
-end
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+ 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :opens, :closes, :menu, :price_range, :category, :total_seats)
+  end
 end
